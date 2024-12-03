6ad781eba14607e62ce1581069a63ad459b39ca1
YegorPanin <66701715+YegorPanin@users.noreply.github.com>
2024-12-03 03:10:12 +0300

    Update main.py
    Добавил асинхронную обработку json, возвращаемого с ответа. (Ollama при запросе с аргументом stream = false отправляет ответы в виде json-обьектов построчно)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import aiohttp
+import os
+import json
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
-import os
-import asyncio, aiohttp
 
 bot_key = os.getenv('BOT_KEY')
 model_name = os.getenv('MODEL_NAME')
@@ -12,15 +13,22 @@ async def llm_reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "model": model_name,
         "prompt": update.message.text
     }
-    await update.message.reply_text("...")
-    answer = "" 
+    message = await update.message.reply_text("...")
+    answer = ""
+
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=payload) as response:
-            data = await response.json()
-            answer += " " + data["response"]
-            await context.bot_edit_message_text(chat_id=update.effective_chat.id, 
-            message_id = message.message_id, 
-            text=answer)
+            if response.status == 200:
+                async for line in response.content:
+                    data = json.loads(line.decode('utf-8'))
+                    answer += " " + data.get("response", "")
+                await context.bot.edit_message_text(chat_id=update.effective_chat.id, 
+                                                    message_id=message.message_id, 
+                                                    text=answer)
+            else:
+                await context.bot.edit_message_text(chat_id=update.effective_chat.id, 
+                                                    message_id=message.message_id, 
+                                                    text=f'Ошибка сервера: {response.status}')
 
 app = ApplicationBuilder().token(bot_key).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, llm_reply))
